ca1d4003fe1191f3c6245685c3495eff0ac37593
kristofdetroch <detroch.kristof@gmail.com>
2022-12-01 08:57:38 +0100

    feat: day01 2 stars

---
+++ day01/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let contents =  include_str!("../input.txt");
+    let mut current_value = 0;
+    let mut calories:Vec<i32> = Vec::new();
+    for line in contents.lines(){
+        if line.is_empty(){
+            calories.push(current_value);
+            current_value = 0;
+        }else{
+            current_value += line.parse::<i32>().unwrap();
+        }
+    }
+    calories.sort();
+    calories.reverse();
+    println!("{}", calories[0]);
+    println!("{}", calories[0..3].iter().sum::<i32>())
+
+}
+
